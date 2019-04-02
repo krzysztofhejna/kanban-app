a181df62bf75be1f06eac3891989aad4859d83ab
krzysztofhejna <khejna15@wp.pl>
2019-04-02 16:37:33 +0200

    Add endpoints for updating and deleting notes

---
+++ server/controllers/note.controller.js
@@ -0,0 +1,73 @@
+import Lane from '../models/lane';
+import Note from '../models/note';
+import uuid from 'uuid';
+
+export function getSomething(req, res) {
+  return res.status(200).end();
+}
+
+export function addNote(req, res) {
+  const { note, laneId } = req.body;
+
+  if (!note || !note.task || !laneId) {
+    res.status(400).end();
+  }
+
+  const newNote = new Note({
+    task: note.task,
+  });
+
+  newNote.id = uuid();
+  newNote.save((err, saved) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    Lane.findOne({ id: laneId })
+      .then(lane => {
+        lane.notes.push(saved);
+        return lane.save();
+      })
+      .then(() => {
+        res.json(saved);
+      });
+  });
+}
+
+export function getNotes(req, res) {
+  Note.find().exec((err, notes) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+  res.json( { notes });
+  });
+}
+
+export function updateNote(req, res) {
+  if (!req.body.note.task) {
+    res.status(403).end();
+  }
+
+  Note.findOneAndUpdate({"id": req.params.noteId}, {"task": req.body.note.task}, function(err, updated) {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.json(updated);
+  })
+}
+
+export function deleteNote(req, res) {
+  Note.findOne( { id: req.params.noteId }).exec((err, note) => {
+
+    if (err) {
+      res.status(500).send(err);
+    }
+
+    lane.findOne({"notes" : { $in: [note._id] } } )
+      .then(lane => {
+        const notesCopy = lane.notes.filter(noteCopy => noteCopy.id !== note.id);
+        lane.notes = notesCopy;
+        return lane.save();
+      })
+      .then(() => note.remove(() => res.status(200).end()));
+  });
+}
---
+++ server/routes/note.routes.js
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import * as NoteController from '../controllers/note.controller';
+
+const router = new Router();
+
+router.route('/notes').post(NoteController.addNote);
+router.route('/notes/:noteId').delete(NoteController.deleteNote);
+router.route('/notes').get(NoteController.getNotes);
+router.route('/notes/:noteId').put(NoteController.updateNote);
+
+export default router;
